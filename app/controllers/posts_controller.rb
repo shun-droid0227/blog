d46563b64c15b4023f4bba2c8304de73b7c60f55
shun-droid0227 <thermohygro1991@gmail.com>
2020-04-19 12:32:51 +0000

    save post title sentence

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,39 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    post = Post.new(post_params)
+    if post.save
+      redirect_to posts_path
+    else
+      render new
+    end
+  end
+
+  def index
+    @post = Post.all
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title,:sentence)
+  end
+
 end
