febaba079efde77872c6f91ff0ed6023f69d6a07
shun-droid0227 <thermohygro1991@gmail.com>
2020-04-19 09:28:32 +0000

    add jQuery

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_18_115154) do
+ActiveRecord::Schema.define(version: 2020_04_19_072622) do
+
+  create_table "posts", force: :cascade do |t|
+    t.text "title"
+    t.text "sentence"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
